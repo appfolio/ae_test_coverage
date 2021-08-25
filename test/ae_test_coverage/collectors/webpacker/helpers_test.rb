bc3b060878617ee8beca1361a8b693390dc66729
Zach Walker <zach.walker@appfolio.com>
2021-08-24 20:36:05 -0700

    zw - adding other webpacker methods to the webpacker collector interface

--- test/ae_test_coverage/collectors/webpacker/helpers_test.rb
+++ test/ae_test_coverage/collectors/webpacker/helpers_test.rb
@@ -29,6 +29,39 @@ module AeTestCoverage
           view.extend(AeTestCoverage::Collectors::Webpacker::Helpers)
           view.render(inline: '<% javascript_packs_with_chunks_tag "foo" %>')
         end
+
+        def test_render__javascript_pack_tag__registers_covered_asset
+          @mock_collector.expects(:add_covered_globs).never
+          view = DummyView.new(::ActionView::LookupContext.new([]), {})
+          view.extend(::Webpacker::Helper)
+          view.render(inline: '<% javascript_pack_tag "foo" %>')
+
+          @mock_collector.expects(:add_covered_globs).with('test/dummy/app/javascript/foo/src/**.{scss,css,js}', 'test/dummy/app/javascript/foo/package*.json')
+          view.extend(AeTestCoverage::Collectors::Webpacker::Helpers)
+          view.render(inline: '<% javascript_pack_tag "foo" %>')
+        end
+
+        def test_render__stylesheet_packs_with_chunks_tag__registers_covered_asset
+          @mock_collector.expects(:add_covered_globs).never
+          view = DummyView.new(::ActionView::LookupContext.new([]), {})
+          view.extend(::Webpacker::Helper)
+          view.render(inline: '<% stylesheet_packs_with_chunks_tag "foo" %>')
+
+          @mock_collector.expects(:add_covered_globs).with('test/dummy/app/javascript/foo/src/**.{scss,css,js}', 'test/dummy/app/javascript/foo/package*.json')
+          view.extend(AeTestCoverage::Collectors::Webpacker::Helpers)
+          view.render(inline: '<% stylesheet_packs_with_chunks_tag "foo" %>')
+        end
+
+        def test_render__stylesheet_pack_tag__registers_covered_asset
+          @mock_collector.expects(:add_covered_globs).never
+          view = DummyView.new(::ActionView::LookupContext.new([]), {})
+          view.extend(::Webpacker::Helper)
+          view.render(inline: '<% stylesheet_pack_tag "foo" %>')
+
+          @mock_collector.expects(:add_covered_globs).with('test/dummy/app/javascript/foo/src/**.{scss,css,js}', 'test/dummy/app/javascript/foo/package*.json')
+          view.extend(AeTestCoverage::Collectors::Webpacker::Helpers)
+          view.render(inline: '<% stylesheet_pack_tag "foo" %>')
+        end
       end
     end
   end
